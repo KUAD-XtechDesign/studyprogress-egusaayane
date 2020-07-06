85fe77fd2be036bb27faa1bcc80025c65cb53baa
egusaayane <a-egusa4271@st.kyoto-art.ac.jp>
2020-07-07 01:11:19 +0900

    adddd

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,13 @@
-$(function(){
-  $('#btn06').on('click',function(){
-      $('#box').slideToggle();//表示非表示をスライドで切り替える
-  })
+// モーダルが複数ある場合
+$(function () {
+  // モーダルのボタンをクリックした時
+  $('.modal_trigger .modal_btn').on('click', function() {
+    var btnIndex = $(this).index(); // 何番目のモーダルボタンかを取得
+    $('.modal_area .modal_box').eq(btnIndex).fadeIn(); // クリックしたモーダルボタンと同じ番目のモーダルを表示する
+  });
+
+  // ×やモーダルの背景をクリックした時
+  $('.modal_close , .modal_bg').click(function(){
+    $('.modal_box').fadeOut(); // モーダルを非表示にする
+  });
 });
